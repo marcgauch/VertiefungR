dc1d3dde78ba1559fc59d4003961b5e511ef8c12
marcgauch <34353267+marcgauch@users.noreply.github.com>
2019-05-06 21:56:50 +0200

    Update R.R

--- Compare-Dedup/R.R
+++ Compare-Dedup/R.R
@@ -12,6 +12,13 @@ tibble(a = 1, b = 1:3)
 # quelle: https://www.bfs.admin.ch/bfs/de/home/statistiken/bevoelkerung/geburten-todesfaelle/vornamen-schweiz.assetdetail.5946318.html
 NAMES_MALE <- c("Daniel", "Peter", "Thomas", "Hans", "Christian", "Martin", "Andreas", "Michael", "Markus", "Marco", "David", "Patrick", "Stefan", "Walter", "Bruno", "Urs", "René", "Marcel", "Roland", "Werner", "Simon", "Pascal", "Beat", "Marc", "Paul", "Kurt", "Roger", "Manuel", "André", "Josef", "Rolf", "Antonio", "Heinz", "Luca", "Rudolf", "Michel", "Robert", "Nicolas", "Christoph", "Jean", "Samuel", "José", "Adrian", "Ernst", "Lukas", "Mario", "Reto", "Philippe", "Fabian", "Alfred", "Matthias", "Alexander", "Philipp", "Stephan", "Pierre", "Jürg", "Florian", "Benjamin", "Jan", "Anton", "Franz", "Giuseppe", "Johann", "Max", "Kevin", "Fabio", "Roman", "Oliver", "Sandro", "Ulrich", "Tobias", "Dominik", "Fritz", "Alain", "Karl", "Jonas", "Olivier", "Gabriel", "Alexandre", "Francesco", "Eric", "Bernhard", "Claudio", "Raphael", "Felix", "Dario", "Noah", "Claude", "Alessandro", "Albert", "Yves", "Richard", "Carlos", "Erich", "Sven", "Robin", "Ivan", "Roberto", "Remo", "Jakob")
 NAMES_FEMALE <- c("Maria", "Anna", "Ursula", "Sandra", "Ruth", "Elisabeth", "Monika", "Claudia", "Verena", "Nicole", "Barbara", "Silvia", "Andrea", "Marie", "Daniela", "Christine", "Karin", "Marianne", "Erika", "Brigitte", "Margrit", "Laura", "Susanne", "Rita", "Sarah", "Katharina", "Esther", "Rosmarie", "Heidi", "Anita", "Manuela", "Doris", "Beatrice", "Sonja", "Rosa", "Yvonne", "Sara", "Jacqueline", "Gertrud", "Ana", "Irene", "Franziska", "Julia", "Cornelia", "Fabienne", "Gabriela", "Martina", "Eva", "Patricia", "Isabelle", "Sabrina", "Nathalie", "Edith", "Alexandra", "Corinne", "Melanie", "Angela", "Nadine", "Alice", "Elena", "Jessica", "Vanessa", "Denise", "Simone", "Anne", "Sophie", "Regula", "Nina", "Caroline", "Emma", "Susanna", "Carmen", "Tanja", "Lara", "Catherine", "Sabine", "Lea", "Petra", "Céline", "Jasmin", "Stefanie", "Therese", "Nadia", "Tamara", "Johanna", "Chantal", "Marina", "Michelle", "Christina", "Martha", "Monique", "Adelheid", "Rahel", "Dora", "Lisa", "Janine", "Hedwig", "Pia", "Anja", "Diana")
-GENDER <- C("m", "f")
 
-function createPerson(gender)
+
+data(RLdata500)
+RLdata500[1:5, ]
+str(RLdata500)
+RLdata500
+data <- as_tibble(RLdata500)
+
+rparis <- compare.dedup(RLdata500, identity = identity.RLdata500)
+rparis$pairs
